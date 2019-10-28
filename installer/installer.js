1fafe8390e293dbbbacba7f391e84a258c19ec24
Joe Klause <joeklause@gmail.com>
2019-10-28 16:09:55 -0700

    small console.log change

--- installer/installer.js
+++ installer/installer.js
@@ -1,17 +1,20 @@
 module.exports = (path, pkgArray, devBool) => {
   const { execSync } = require('child_process');
-  console.log('Dependencies are loading');
+
   if(devBool) {
+    console.log('devDependencies are loading');
     pkgArray.forEach(pkg => execSync(`npm i -D ${pkg}`, {
       cwd: path,
       stdio: 'inherit'
     }));
+    console.log('devDependencies finished loading');
   } 
   else {
+    console.log('Dependencies are loading');
     pkgArray.forEach(pkg => execSync(`npm i ${pkg}`, {
       cwd: path,
       stdio: 'inherit'
     }));
+    console.log('Dependencies finished loading');
   }
-  console.log('Dependencies finished loading');
 };
